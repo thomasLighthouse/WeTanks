c66b5e1f87c1bc65dee5e500ed271f3a556cebd2
alexbanta4 <alexbanta4@gmail.com>
2020-12-01 03:03:08 -0600

    Bullet Rotation/Collision, Dynamic Walls, and Menu Prefabs
    Still need to add menu code

--- Assets/Code/Shell.cs
+++ Assets/Code/Shell.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private Vector2 v; // velocityBeforePhysicsUpdate;
     public float speed = 2f;
     private int bounces = 0;
     // Start is called before the first frame update
@@ -12,22 +13,53 @@ public class Shell : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _rb.velocity = speed * transform.up;
+        v = _rb.velocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is called once per physics step
+    void FixedUpdate()
     {
-
+        v = _rb.velocity;
     }
 
-    internal void OnCollisionEnter2D (Collision2D other) {
-
-        if (other.gameObject.name.Contains("Wall")){
-            Debug.Log("WALL");
+    internal void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Contains("Player"))
+        {
+            Destroy(gameObject);
         }
 
-        if (other.gameObject.name.Contains("Player")){
-            Destroy(gameObject);
+        else
+        {
+            GameObject otherObject = other.gameObject;
+            if ((otherObject.GetComponent<Transform>().position.x - otherObject.GetComponent<Renderer>().bounds.extents.x) >= this.GetComponent<Transform>().position.x && v.x > 0)
+            {
+                _rb.velocity = new Vector2(-v.x, v.y);
+                bounces++;
+            }
+            else if ((otherObject.GetComponent<Transform>().position.x + otherObject.GetComponent<Renderer>().bounds.extents.x) <= this.GetComponent<Transform>().position.x && v.x < 0)
+            {
+                _rb.velocity = new Vector2(-v.x, v.y);
+                bounces++;
+            }
+
+            if ((otherObject.GetComponent<Transform>().position.y - otherObject.GetComponent<Renderer>().bounds.extents.y) >= this.GetComponent<Transform>().position.y && v.y > 0)
+            {
+                _rb.velocity = new Vector2(v.x, -v.y);
+                bounces++;
+            }
+            else if ((otherObject.GetComponent<Transform>().position.y + otherObject.GetComponent<Renderer>().bounds.extents.y) <= this.GetComponent<Transform>().position.y && v.y < 0)
+            {
+                _rb.velocity = new Vector2(v.x, -v.y);
+                bounces++;
+            }
+            // Rotate bullet
+            transform.up = _rb.velocity;
+            _rb.angularVelocity = 0;
+            if (bounces > 2)
+            {
+                Destroy(gameObject);
+            }
         }
 
     }
---
+++ Assets/Code/WallSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    private Camera Cam;
+    private Vector2 BottomLeft;
+    private Vector2 BottomRight;
+    private Vector2 TopLeft;
+    private Vector2 TopRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cam = Camera.main; 
+        BottomLeft = Cam.ViewportToWorldPoint(new Vector2(0, 0));
+        BottomRight = Cam.ViewportToWorldPoint(new Vector2(1, 0));
+        TopLeft = Cam.ViewportToWorldPoint(new Vector2(0, 1));
+        TopRight = Cam.ViewportToWorldPoint(new Vector2(1, 1));
+        Debug.Log(TopRight);
+        GameObject _wallPrefab = (GameObject)Resources.Load("WoodWall");
+        GameObject bottomWall = GameObject.Instantiate(_wallPrefab, (BottomLeft + BottomRight) / 2, new Quaternion());
+        bottomWall.transform.localScale = new Vector3(TopRight.x / 2, bottomWall.transform.localScale.y,1);
+
+        GameObject rightWall = GameObject.Instantiate(_wallPrefab,(BottomRight + TopRight) / 2, new Quaternion());
+        rightWall.transform.localScale = new Vector3(rightWall.transform.localScale.x, TopRight.y, 1);
+
+        GameObject topWall = GameObject.Instantiate(_wallPrefab, (TopRight + TopLeft) / 2, new Quaternion());
+        topWall.transform.localScale = new Vector3(TopRight.x / 2, topWall.transform.localScale.y, 1);
+
+        GameObject leftWall = GameObject.Instantiate(_wallPrefab, (TopLeft + BottomLeft) / 2, new Quaternion());
+        leftWall.transform.localScale = new Vector3(leftWall.transform.localScale.x, TopRight.y, 1);
+        //Debug.Log(Screen.width);
+        //Debug.Log(Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
